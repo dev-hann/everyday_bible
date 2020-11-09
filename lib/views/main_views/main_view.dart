777a8f607deb7a996e82328704f44a20ec649498
dev-hann <developer.hann@gmail.com>
2020-11-09 19:23:09 +0800

    notification fix!!

--- lib/views/main_views/main_view.dart
+++ lib/views/main_views/main_view.dart
@@ -9,24 +9,13 @@ class EveryDayBible extends StatefulWidget {
 
 class _EveryDayBibleState extends State<EveryDayBible>
     with TickerProviderStateMixin {
-  BibleController _bibleController = BibleController();
+  BibleController _bibleController;
 
-  Widget _title() {
-    return Text.rich(
-      TextSpan(
-        children: [
-          TextSpan(text: _bibleController.title + "\n"),
-          TextSpan(
-              text: _bibleController.subTitle,
-              style: Theme.of(context).textTheme.subtitle2),
-        ],
-      ),
-      style: Theme.of(context)
-          .textTheme
-          .headline4
-          .copyWith(fontWeight: FontWeight.bold),
-    );
+  void initState(){
+    super.initState();
+    _bibleController = Provider.of<BibleController>(context,listen: false);
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -45,17 +34,9 @@ class _EveryDayBibleState extends State<EveryDayBible>
         ),
         child: Column(
           children: [
-            SizedBox(
-              height: ScreenUtil().screenHeight / 5,
-              child: Align(
-                alignment: Alignment(-0.8, 0.3),
-                child: _title(),
-              ),
-            ),
-            Expanded(child: GospelListView()),
-            BottomPlayer(
-              url: _bibleController.audio,
-            )
+            MainViewTitle(),
+            MainViewBodyList(),
+            MainViewBottomPlayer(),
           ],
         ),
       ),
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -3,13 +3,14 @@ library views;
 
 import 'package:everydaybible/controller/bible_audio_controller.dart';
 import 'package:everydaybible/controller/bible_controller.dart';
-import 'package:everydaybible/models/bible.dart';
 import 'package:everydaybible/utils/animations/page_animation.dart';
+import 'package:everydaybible/utils/bible_notification.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
-part 'bottom_player.dart';
-part 'main_view.dart';
-part 'gospels_list_view.dart';
+part 'main_views/main_view_bottom_player.dart';
+part 'main_views/main_view.dart';
+part 'main_views/main_view_body_list.dart';
+part 'main_views/main_view_title.dart';
 part 'splash.dart';
