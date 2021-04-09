a869e401c4283e36338db3b8ebc630d2d46b18fc
yoehwan <yoehwan@gmail.com>
2021-04-09 08:09:36 +0800

    fix bottom flexible

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,17 @@ import 'package:everydaybible/utils/data_loader/web_data_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+
 // import 'package:firebase_core/firebase_core.dart'
 import 'views/view_lib.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -22,23 +22,24 @@ class MyApp extends StatelessWidget {
           create: (_) => BibleDatabase(),
         ),
       ],
-      child:GetMaterialApp(
+      child: GetMaterialApp(
         title: 'EveyDay Bible',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          accentColor: Colors.brown.shade700,
+          accentColor: Colors.brown[700],
           primarySwatch: Colors.brown,
-          cardColor: Colors.brown.shade900,
+          cardColor: Colors.brown[900],
           iconTheme: IconThemeData(color: Colors.white),
           textTheme: TextTheme(
-              bodyText2: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold)),
+            bodyText2: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-     // home: Text("!!"),
-       home: LoadingView(),
+        home: LoadingView(),
       ),
     );
   }
