9d301c8d07f489108e6e50566d9a999e32f63973
dev-hann <developer.hann@gmail.com>
2021-04-10 17:23:00 +0800

    add dialog

--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -1,15 +1,55 @@
 part of view_model;
 
 class MainViewModel extends BibleViewModel {
-  MainViewModel(this.database);
+  MainViewModel(this.database) {
+    _loadBibleFromDB();
+    this.database.bibleNotifier.addListener(() {
+      _loadBibleFromDB();
+      loading = false;
+      notifyListeners();
+    });
+  }
 
- late final BibleDatabase database;
+  final BibleDatabase database;
 
-  String get title => database.selectedDateBible.title;
+  Bible? _selectedBible;
 
-  String get subtitle => database.selectedDateBible.subTitle;
+  void _loadBibleFromDB() {
+    _selectedBible = database.bibleNotifier.value!;
+  }
 
-  Map<String, String> get gospels => database.selectedDateBible.gospels;
+  String get title => _selectedBible!.title;
 
-  String get audioAsset => database.selectedDateBible.audio;
+  String get subtitle => _selectedBible!.subTitle;
+
+  Map<String, String> get gospels => _selectedBible!.gospels;
+
+  String get audioAsset => _selectedBible!.audio;
+
+  String get dateTime => _selectedBible!.dateTime;
+
+  bool loading = false;
+
+  ScrollController gospelScrollController = ScrollController();
+
+  DateTime get _dateTime {
+    List<int> dateList = dateTime.split("-").map((e) => int.parse(e)).toList();
+    return DateTime(dateList[0], dateList[1], dateList[2]);
+  }
+
+  void onTapYesterday() async {
+    loading = true;
+    notifyListeners();
+    await database.loadData(_dateTime.add(Duration(days: -1)));
+    gospelScrollController.jumpTo(0.0);
+  }
+
+  bool get hasTomorrow => !(_dateTime.difference(DateTime.now()).inDays > 5);
+
+  void onTapTomorrow() async {
+    loading = true;
+    notifyListeners();
+    await database.loadData(_dateTime.add(Duration(days: 1)));
+    gospelScrollController.jumpTo(0.0);
+  }
 }
